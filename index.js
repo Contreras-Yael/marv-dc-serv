6bd5447ea5070ca3c284ca86c307fe6346b3bb0e
Contreras Yael <85694235+Contreras-Yael@users.noreply.github.com>
2026-06-29 00:35:43 -0700

    tengo mieod

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require("cors");
-const helmet = require('helmet');
 const { heroes_api } = require('./database/connection');
 
 const app = express();
@@ -9,24 +8,8 @@ const PORT = 3977;
 
 app.use(cors());
 
-// app.use(
-//   helmet({
-//     crossOriginResourcePolicy: false,
-//   })
-// );
-
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
-app.get('/api/heroes/general', async(req, res) => {
-try{
-const personaje = await heroes_api();
-res.json(personaje);
-
-}  catch(error){
-    res.status(500).json({mensaje: 'No funciona, carga de personaje'});
-}
-});
 
 app.get('/api/heroes', async(req, res) => {
 try{
